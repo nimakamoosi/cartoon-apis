0e638a9a3d88f50f386569619e3a6eca6ea68b6b
Nima Kamoosi <nimakamoosi@microsoft.com>
2022-05-27 11:14:58 -0400

    Enabling swagger on release deployment and minor fixes.

--- DataAccess/ApplicationDbContext.cs
+++ DataAccess/ApplicationDbContext.cs
@@ -13,16 +13,16 @@ namespace CartoonApis.DataAccess
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             // Families
-            Family FlinststoneFamily = new Family(id: 1, name: "Flinststones");
+            Family FlintstoneFamily = new Family(id: 1, name: "Flintstones");
             Family rubbleFamily = new Family(id: 2, name: "Rubbles");
             Family smithFamily = new Family(id: 3, name: "Smiths");
-            Family simpsonFamily = new Family(id: 3, name: "Simpsons");
-            modelBuilder.Entity<Family>().HasData(FlinststoneFamily, rubbleFamily, smithFamily, simpsonFamily);
+            Family simpsonFamily = new Family(id: 4, name: "Simpsons");
+            modelBuilder.Entity<Family>().HasData(FlintstoneFamily, rubbleFamily, smithFamily, simpsonFamily);
 
             // People
-            Person fredFlinststone = new Person(id: 1, firstName: "Fred", lastName: "Flinststone", familyId: FlinststoneFamily.Id);
-            Person wilmaFlinststone = new Person(id: 2, firstName: "Wilma", lastName: "Flinststone", familyId: FlinststoneFamily.Id);
-            Person pebblesFlinststone = new Person(id: 3, firstName: "Pebbles", lastName: "Flinststone", familyId: FlinststoneFamily.Id);
+            Person fredFlintstone = new Person(id: 1, firstName: "Fred", lastName: "Flintstone", familyId: FlintstoneFamily.Id);
+            Person wilmaFlintstone = new Person(id: 2, firstName: "Wilma", lastName: "Flintstone", familyId: FlintstoneFamily.Id);
+            Person pebblesFlintstone = new Person(id: 3, firstName: "Pebbles", lastName: "Flintstone", familyId: FlintstoneFamily.Id);
             Person barneyRubble = new Person(id: 4, firstName: "Barney", lastName: "Rubble", familyId: rubbleFamily.Id);
             Person bettyRubble = new Person(id: 5, firstName: "Betty", lastName: "Rubble", familyId: rubbleFamily.Id);
             Person bammbammRubble = new Person(id: 6, firstName: "Bamm-Bamm", lastName: "Rubble", familyId: rubbleFamily.Id);
@@ -34,12 +34,12 @@ namespace CartoonApis.DataAccess
             Person bethSmith = new Person(id: 11, firstName: "Beth", lastName: "Smith", familyId: smithFamily.Id);
 
             Person homerSimpson = new Person(id: 12, firstName: "Homer", lastName: "Simpson", familyId: simpsonFamily.Id);
-            Person margeSimpson = new Person(id: 12, firstName: "Marge", lastName: "Simpson", familyId: simpsonFamily.Id);
-            Person bartSimpson = new Person(id: 12, firstName: "Bart", lastName: "Simpson", familyId: simpsonFamily.Id);
-            Person lisaSimpson = new Person(id: 12, firstName: "Lisa", lastName: "Simpson", familyId: simpsonFamily.Id);
-            Person maggieSimpson = new Person(id: 12, firstName: "Maggie", lastName: "Simpson", familyId: simpsonFamily.Id);
+            Person margeSimpson = new Person(id: 13, firstName: "Marge", lastName: "Simpson", familyId: simpsonFamily.Id);
+            Person bartSimpson = new Person(id: 14, firstName: "Bart", lastName: "Simpson", familyId: simpsonFamily.Id);
+            Person lisaSimpson = new Person(id: 15, firstName: "Lisa", lastName: "Simpson", familyId: simpsonFamily.Id);
+            Person maggieSimpson = new Person(id: 16, firstName: "Maggie", lastName: "Simpson", familyId: simpsonFamily.Id);
 
-            modelBuilder.Entity<Person>().HasData(fredFlinststone, wilmaFlinststone, pebblesFlinststone,
+            modelBuilder.Entity<Person>().HasData(fredFlintstone, wilmaFlintstone, pebblesFlintstone,
                 barneyRubble, bettyRubble, bammbammRubble,
                 rickSanchez, mortySmith, summerSmith, jerrySmith, bethSmith,
                 homerSimpson, margeSimpson, bartSimpson, lisaSimpson, maggieSimpson
